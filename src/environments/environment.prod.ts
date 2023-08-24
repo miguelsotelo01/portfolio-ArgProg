745532a17ed0ce948970ab7809c45f5801a57bc6
Miguel Angel Sotelo <93306022+miguelsotelo01@users.noreply.github.com>
2023-08-24 16:59:23 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://agile-basin-59013.herokuapp.com',
-  apiAuthUrl: 'https://agile-basin-59013.herokuapp.com/auth/'
+  apiBaseUrl: 'https://backend-ap-jolo.onrender.com',
+  apiAuthUrl: 'https://backend-ap-jolo.onrender.com/auth/'
 };
